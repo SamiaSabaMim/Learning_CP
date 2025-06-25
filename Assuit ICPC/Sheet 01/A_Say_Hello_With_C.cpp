d151cd18fdb4df3c0e58a2c06f5a8cd7bc29cf26
SamiaSabaMim <samiatusesabamim@gmail.com>
2025-06-25 20:20:30 +0600

    Sheet 01 , Problem A

--- Assuit ICPC/Sheet 01/A_Say_Hello_With_C.cpp
+++ Assuit ICPC/Sheet 01/A_Say_Hello_With_C.cpp
@@ -1,1 +1,8 @@
 #include <iostream>
+using namespace std;
+int main(){
+    char a[1000]; 
+    cin >> a;
+    cout << "Hello, " << a;
+}
+
---
+++ Assuit ICPC/Sheet 01/C_Simple_Calculator.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int X, Y; cin >> X >> Y ;
+    cout << X << " + " << Y << " = " << X + Y <<'\n';
+    cout << X << " * " << Y << " = " << X * Y <<'\n';
+    cout << X << " - " << Y << " = "  << X - Y <<'\n';
+}
