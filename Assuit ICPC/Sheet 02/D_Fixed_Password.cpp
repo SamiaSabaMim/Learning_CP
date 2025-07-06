aa8a3fe1842c551e9c952514f92092efe10b0178
SamiaSabaMim <samiatusesabamim@gmail.com>
2025-07-06 22:06:10 +0600

    Sheet 02 Problem D

---
+++ Assuit ICPC/Sheet 02/D_Fixed_Password.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int X;
+    for(;;){ cin>>X;
+    if(X==1999)
+    {
+        cout<<"Correct"<<'\n';
+        break;
+    }
+    else{
+        cout<<"Wrong"<<'\n';
+    }
+}
+
+   return 0;
+}
+
