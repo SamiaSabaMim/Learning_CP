515fea0c8a357b931ecba4a862095fe789b4cc22
SamiaSabaMim <samiatusesabamim@gmail.com>
2025-07-07 19:21:16 +0600

    Sheet 02 Problem E

---
+++ Assuit ICPC/Sheet 02/E_Max.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N; cin >> N;
+    int a[N];
+    for(int i=0; i<N; i++){
+    cin >> a[i];
+    }
+    int max = 0;
+    for(int i=0; i<N; i++){
+    if(a[i] > max)
+    {
+        max = a[i];
+    }
+}
+    cout<< max <<'\n';
+}
